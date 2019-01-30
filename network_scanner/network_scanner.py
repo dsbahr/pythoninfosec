3c5cd26c4144f096f5c17184a46b0bb3ecb0e52f
root <root@localhost.localdomain>
2019-01-30 16:02:14 -0500

    Network scanner refactorings

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,20 +1,45 @@
 #!/usr/bin/env python
-
 import scapy.all as scapy
+import argparse
 
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
-
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("----------------------------------------")
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n")
+    print("----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
 
 def simple_scan(ip):
     scapy.arping(ip)
 
-scan("10.0.2.1/24")
+
+def create_argument_parser(argument_name):
+    argument_parser = argparse.ArgumentParser(description="Get IP address range")
+    argument_parser.add_argument("--" + argument_name)
+    return argument_parser
+
+
+def parse_ip_from_input(parser):
+    args = parser.parse_args()
+    return args.target
+
+
+ip_range = parse_ip_from_input(
+    create_argument_parser(
+        argument_name="target"
+    ))
+scan_result = scan(ip_range)
+print_result(scan_result)
